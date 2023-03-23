493f5319ccdce108a910cbba01afd89a3c4a91c6
Lassiaa <lassiaa@metropolia.fi>
2023-03-23 09:43:44 +0200

    Routing home and profile

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,8 +1,22 @@
 import './App.css';
-import MediaTable from './components/MediaTable';
+import Home from './views/Home';
+import Single from './views/Single';
+import Profile from './views/Profile';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Layout from './views/Layout';
 
 const App = () => {
-  return <MediaTable />;
+  return (
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Single />} />
+          <Route path="/" element={<Profile />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
